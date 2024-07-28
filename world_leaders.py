04a44a735b18a8f5e73525b7cb2197f6ba34143b
sofaca <sofia1cstcst@gmail.com>
2024-07-28 11:56:02 -0300

    Cleaning the data, session_1

--- world_leaders.py
+++ world_leaders.py
@@ -13,18 +13,25 @@ i will punch you in the face ok?? (ง'̀-'́)ง
 #### 1.Loading the data ####
 
 import pandas as pd
+import numpy as np
 
 # URL to the data file
-file_path =r"C:\Users\sofia\Desktop\sofa\proyectos\python\worldleaders\arch_annual.txt"
+file_path =r"C:\Users\sofia\Desktop\sofa\proyectos\python\worldleaders\world-leaders\arch_annual.txt"
 
 data = pd.read_csv(file_path, delimiter='\t', encoding='latin1')
 
+
+
+
+
+
 #### 2.Exploring the data ####
+
 # Display the first few rows to understand its structure
 # print(data.head())
 
 # Display the basic information about the data set
-print (data.info())
+# print (data.info())
 
 # Displayed informations is:
     # "RangeIndex: 17686 entries, 0 to 17685
@@ -61,14 +68,53 @@ print (data.info())
 
 # What is this telling me about my code?
 
-#Non-null, as oppossite of NaN, stands for the rows 
-#with completed information. If the Non-Null count matches
-#the number of rows meands absolute completeness.
+#   Non-null, as oppossite of NaN, stands for the rows 
+#   with completed information. If the Non-Null count matches
+#   the number of rows meands absolute completeness.
 
-#This dataset is incomplete in borndate, deathdate (probably sons
-#of bitches still alive), dbpedia.uri (link a wikipedia), fties (family ties), f
-#ftcur (0 or 1 wether the fties is to a past or future leader)
+#   This dataset is incomplete in borndate, deathdate (probably sons
+#   of bitches still alive), dbpedia.uri (link a wikipedia), fties (family ties), f
+#   ftcur (0 or 1 wether the fties is to a past or future leader)
 
 # print (data.describe())
-# print (data.columns)
+##  ↑ Displays basic statistic for distribution and tendencies
+#   info on countable values, 
+#   count de no-nulls, promedio, desvío estándar, mín-max
+#   quintiles.
 
+# print (data.columns)
+##  ↑ Displays the columns of the data set
+
+# print (data["gender"].value_counts())
+##  ↑ Displays the amount of occurrences/time in a row,
+#   for different values. 
+#   ya con esta línea se puede filtrar y observar 
+#   concurrencias en el código específicas. Tipo
+#   "Gender", cuántos M y cuantos F.
+
+
+
+
+
+#### 3.Cleaning the data ####
+
+#Para hacer la data más legible.
+
+#   Reemplazando los valores nulos de las fecha (-999) por
+#   nodata
+data["yrborn"] = data["yrborn"].replace([-999,-777], np.nan)
+data['yrdied'] = data['yrdied'].replace([-999, -777], np.nan)
+
+#   Ahora vamo a hacer que los años aparezcan como integers
+# en vez de floats → pd.to_numeric 
+
+#   Pero que los Nan permanezcan Nan for aesthetic reasons,
+#    en vez de ponerle un valor → errors= "coerce"
+#   Esto lo que dice es "en la conversión a ints, si aparece un error
+#   un valor no numérico, que sean forzados a ser Nan, y no salte ningún
+#   error.
+data ["yrborn"] = pd.to_numeric(data["yrborn"], errors= "coerce").astype("Int64")
+data["yrdied"] = pd.to_numeric(data["yrdied"], errors= "coerce"). astype("Int64")
+
+# print (data[["yrborn", "yrdied"]].head())
+print (data["yrdied"].value_counts())
